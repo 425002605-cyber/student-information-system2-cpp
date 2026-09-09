11dc81d2bf6fd5adc1ea82f1cbca27b6eee095e8
425002605-cyber <425002605@ntc.edu.ph>
2026-09-09 13:45:59 +0800

    Add student ID and email

--- student information system2.cpp
+++ student information system2.cpp
@@ -9,6 +9,8 @@ cout << endl;
 cout << "Name: Jinea Elgin M. Benjamin" << endl;
 cout << "Course: BSIT" << endl;
 cout << "Year Level: 2" << endl;
+cout << "Student ID: 2026-001" << endl;
+cout << "Email: 425002605@ntc.edu.ph" << endl;
 cout << endl;
 cout << "Welcome to the system!" << endl;
 return 0;

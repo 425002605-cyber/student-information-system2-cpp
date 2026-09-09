14ecfcf5f71769af54f436094914a371d384d2d7
425002605-cyber <425002605@ntc.edu.ph>
2026-09-09 13:32:20 +0800

    initial C++ student information system2

---
+++ student information system2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main() {
+
+cout << "==============================" << endl;
+cout << " STUDENT INFORMATION SYSTEM" << endl;
+cout << "==============================" << endl;
+cout << endl;
+cout << "Name: Jinea Elgin M. Benjamin" << endl;
+cout << "Course: BSIT" << endl;
+cout << "Year Level: 2" << endl;
+cout << endl;
+cout << "Welcome to the system!" << endl;
+return 0;
+}
